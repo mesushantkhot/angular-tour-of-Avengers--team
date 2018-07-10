fe49af0d2df1e1e9bf4ce4b3314109da668b7f4f
Sushant Khot <mesushantkhot@gmail.com>
2018-07-10 15:55:35 +0530

    Initial commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Captain America' },
+  { id: 12, name: 'Iron Man' },
+  { id: 13, name: 'Hulk' },
+  { id: 14, name: 'Black Widow' },
+  { id: 15, name: 'Thor' },
+  { id: 16, name: 'SpiderMan' },
+  { id: 17, name: 'Black Panther' },
+  { id: 18, name: 'Vision' },
+  { id: 19, name: 'Nick Fury' },
+  { id: 20, name: 'Hawk Eye' }
+];
